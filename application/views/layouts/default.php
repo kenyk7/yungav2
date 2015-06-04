39af4a7d09546b718098c72469a0701f1fe72f41
kenyk7 <kenykalin@gmail.com>
2015-06-03 19:13:30 -0500

    Edit app and gs

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -169,106 +169,15 @@
 <script src="<?php echo base_url(); ?>assets/plugins/pnotify/pnotify.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/jasnyupload/jasny-bootstrap.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/Kajax.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/app.js"></script>
 <script>
-  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  // ga('create', 'UA-60298232-2', 'auto');
-  // ga('send', 'pageview');
-
-
-$(function () {
-  $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-          $('.navbar').addClass('navbar-dark');
-      } else {
-          $('.navbar').removeClass('navbar-dark');
-      }
-  });
-});
-
-var cur = location.href;
-if($(".actives a[href='"+cur+"']").attr('href') == cur){
-    $(".actives a[href='"+cur+"']").addClass('active');
-    var parent = $(".actives a[href='"+cur+"']").parent();
-    parent.addClass('active');
-}
-
-$('.open-share').click(function(e){
-  e.preventDefault();
-  $(this).parents('.social-container').children('.btn-social').toggleClass('right-normal');
-});
-
-$('.btn-social-close').click(function(e){
-  e.preventDefault();
-  $(this).parents('.btn-social').toggleClass('right-normal');
-});
-
-jQuery(document).ready(function($) {
-  var forms = new Kajax();
-  forms.sendFormFile('#form-image-upload');
-});
-
-wow = new WOW().init();
-
-$(function() {
-  $('#blueimp-gallery').on('opened', function (event) {
-      $('.slides img').okzoom({
-        width: 250,
-        height: 250,
-        round: true,
-        background: "#fff",
-        backgroundRepeat: "repeat",
-        border: "2px solid #ddd"
-      });
-  });
-  // photo indiviadual
-  $('.figure-center img').okzoom({
-      width: 250,
-      height: 250,
-      round: true,
-      background: "#fff",
-      backgroundRepeat: "repeat",
-      border: "2px solid #ddd"
-    });
-});
-
-$(function () {
-
-  $(".progress").fadeOut();
-  $('[data-toggle="tooltip"]').tooltip();
-  // init typed
-  var type = $(".welcome").text();
-  $(".welcome").typed({
-    strings: [type],
-    typeSpeed: 50,
-    contentType: 'html',
-    cursorChar: "",
-  });
-
-  $(".social-actions [data-action]").click(function(e) {
-    e.preventDefault();
-    var clickthis = $(this);
-    $.get($(this).data('action'), function(data) {
-      if(data['message'] === 'success'){
-          new PNotify({title: 'Ok!',text: 'Gracias por el Like',type: 'success',styling: 'fontawesome'});
-          var numlikes = clickthis.parents('.social-actions').children('.likes');
-          numlikes.text(Number(numlikes.text())+1);
-
-      }else{
-        new PNotify({title: 'Info',text: 'Al parecer ya indicó que le gusta esta Foto',type: 'info',styling: 'fontawesome'});
-      }
-    });
-  });
-
- var $K = $('.isotope-content');
-  $K.imagesLoaded(function () {
-    $K.masonry({itemSelector:'.item', isAnimated:true,layoutMode: 'masonry'});
-  });
-
-})
+  ga('create', 'UA-60298232-2', 'auto');
+  ga('send', 'pageview');
 </script>
 </body>
 </html>
